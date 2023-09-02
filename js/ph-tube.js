b552898865f92d90700acd12dc3c249f99f01b10
Pinku1211 <pinkukumarsarker@gmail.com>
2023-09-02 16:31:13 +0600

    sorting completed

--- js/ph-tube.js
+++ js/ph-tube.js
@@ -1,39 +1,34 @@
 const handleCategory = async () => {
     const response = await fetch("https://openapi.programming-hero.com/api/videos/categories")
     const data = await response.json()
-
     const tabContainer = document.getElementById("tab-container")
-
     data.data.forEach(category => {
         const div = document.createElement("div")
         div.innerHTML = `
-        <button onclick="handleLoadVideos('${category.category_id}')" class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg bg-gray-200 rounded-sm lg:rounded-md hover:bg-[#f35d71] normal-case">${category.category}</button>
-
+        <button onclick="handleLoadVideos('${category.category_id}'); sortByViewId('${category.category_id}')" class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg bg-gray-200 rounded-sm lg:rounded-md hover:bg-[#f35d71] hover:text-white focus:bg-[#f35d71] focus:text-white normal-case">${category.category}</button>
         `;
         tabContainer.appendChild(div)
-
-    });
-
-
+        
+    });  
 }
 
 const handleLoadVideos = async (categoryId) => {
-
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`)
-    const data = await response.json()
-    
+    const data = await response.json()   
     const warningContainer = document.getElementById("warning-container")
-    
     if(data.data.length === 0) {
         warningContainer.classList.remove("hidden")
     }
     else{
         warningContainer.classList.add("hidden")
     }
+    displayVideos(data.data)
+}    
+
+const displayVideos = (dataArray) => {
     const cardContainer = document.getElementById("card-container");
     cardContainer.textContent = " "
-    data.data.forEach(video => {
-
+    dataArray.forEach(video => {
         const div = document.createElement("div")
         div.innerHTML = `
         <div class="card">
@@ -44,7 +39,6 @@ const handleLoadVideos = async (categoryId) => {
                 <p class="text-white text-[10px] bg-black">${timeConverter(video.others.posted_date)}</p>
             </div>
         </div> 
-
         <div class="flex gap-3 mt-8">
             <img class="h-14 w-14  rounded-full" src='${video.authors[0].profile_picture}' alt="">
             <div class="space-y-3">
@@ -58,14 +52,10 @@ const handleLoadVideos = async (categoryId) => {
         </div>
     </div>
         `
-
     cardContainer.appendChild(div)
-
     })
-
-
 }
-
+    
 const timeConverter = (sec) => {
     if(!sec) {
         return '';
@@ -75,24 +65,42 @@ const timeConverter = (sec) => {
         const min = Math.floor((sec - (hrs * 3600)) / 60);
         if (hrs >= 24) {
             const days = hrs / 24;
-            if (days > 360) {
-                const years = Math.floor(days / 360);
+            if (days > 365) {
+                const years = Math.floor(days / 365);
                 return `${years} years ago`
             }
             return `${days} days ago`
         }
         return `${hrs} hrs ${min} min ago`; 
     }
-
 }
 
 const openBlog = () => {
     window.location.href = "blog.html";
 }
-const goHome = () => {
-    window.location.href = "index.html"
+
+const sortByViewId = async (categoryId) => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`)
+    const data = await response.json()
+    dataSorting(data)
+}
+
+const dataSorting = (data) => {
+    const dataArray = data.data;
+    dataArray.sort((a, b) => {
+        const parseValue = (str) => {
+          const viewsNumber = parseFloat(str) * 1000;
+          return viewsNumber;
+        };
+        const aValue = parseValue(a.others.views);
+        const bValue = parseValue(b.others.views);
+        return bValue - aValue;
+      });
+      document.getElementById("sort-by-view").addEventListener("click", function(){
+        displayVideos(dataArray)
+      })
+
 }
 
 handleLoadVideos("1000")
 handleCategory()
-
